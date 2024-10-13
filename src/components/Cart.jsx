cdaa94f02e42beb7e22e68c736727f0d38e24890
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2024-10-13 23:48:30 +0600

    updated cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { products } from '../../public/products';
+import { useCartStore } from '../store/cartStore';
+import { LuX } from 'react-icons/lu';
 
 export default function Cart() {
   const allProducts = products;
+  // const {cart, removeFromCart, clearCart} = useCartStore((state) => ({
+  //   cart: state.cart,
+  //   removeFromCart: state.removeFromCart,
+  //   clearCart: state.clearCart
+  // }));
   return (
     <div className='cart-page'>
+      <button className='clear-cart'>Clear Cart</button>
       <div className="cart-info">
         {
           allProducts.map(product =>
@@ -13,6 +21,7 @@ export default function Cart() {
               <p className="total-price product-item">Price: <span>00</span></p>
               <p className='item-count product-item'>Total Added: <span>00</span></p>
               <p className='total-cost product-item'>Total Cost: <span>00</span></p>
+              <div className="remove-from-cart"><LuX className='icon' /></div>
             </div>
           )
         }
---
+++ src/store/cartStore.jsx
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+
+export const useCartStore = create((set) => ({
+  cart: [],
+  addToCart: (product) => set((state) => ({ cart: [...state.cart, product] })),
+  removeFromCart: (productId) => set((state) => ({ cart: state.cart.filter(product => product.id !== productId) })),
+  clearCart: () => set({ cart: [] })
+}));
+
